22a3e429ac0990938a9afaed4ace26205fae7729
Tehc <noahsturner06@gmail.com>
2019-05-20 18:45:54 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,27 @@
+//MPPBL(Multiplayer Piano Bot Library) By: ๖ۣۜTehc
+	var blob = new Blob(["https://pastebin.com/raw/0RLP8qi2"], {type : "text/plain"});
+	var reader = new FileReader();
+	var ver = reader.readAsText(blob);
+	setTimeout(() => {
+		let script = document.createElement('script');
+		script.src = 'https://cdn.jsdelivr.net/gh/TehcJS/MPPBL@a7695a6/mppbl.js';
+		document.body.appendChild(script);
+	}, 1000);
+//***********************************************//
+
 prefix = '&';
 bot_name = 'MPP Bot';
 room_name = 'lobby';
 
-createCommand('help', (args, user) => {
+createCommand({name: 'help', desc: 'Show a list of commands'}, (args, user) => {
 	let cmdList = [];
 	Object.keys(commands).forEach((cmd, i) => {
-		cmdList.push(commands[cmd]);
+		cmdList.push(commands[cmd].name);
 	});
 	sendMsg(`Commands: ${cmdList.join(', ')}`);
 });
 
-createCommand('js', (args, user) => {
+createCommand({name: 'js', desc: 'Evaluates javascript code'}, (args, user) => {
 	try {
 		let code = JSON.stringify(eval(args));
 		if (code == undefined) {
@@ -24,6 +35,6 @@ createCommand('js', (args, user) => {
 	}
 });
 
-createCommand('hi', (args, user) => {
+createCommand({name: 'hi', desc: 'A basic command example', (args, user) => {
 	sendMsg(`Hi ${user.name}!`);
 });
